85af57b5cfaba4f30552a001f5f688864344caa7
PACFWL <estudanteprincipalgerali@gmail.com>
2024-12-02 21:39:19 -0300

    feature: "new  version"

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import unicodedata
 import csv
 import numpy as np
 from collections import Counter
-from nltk.corpus import stopwords 
+from nltk.corpus import stopwords
 import nltk
+
 nltk.download('stopwords')
 
 app = Flask(__name__)
 
 
 with open('model.pkl', 'rb') as model_file:
-    model, vocab = pickle.load(model_file)
+    model, vocab, label_encoder = pickle.load(model_file)
 
 
 def load_emoji_map(filepath):
@@ -23,7 +24,6 @@ def load_emoji_map(filepath):
         for row in reader:
             emoji_map[row['emoji']] = row['sentimento']
     return emoji_map
-
 
 def load_giria_map(filepath):
     giria_map = {}
@@ -39,18 +39,14 @@ def replace_emojis(text, emoji_map):
         text = text.replace(emoji, description)
     return text
 
-
 def replace_girias(text, giria_map):
     words = text.split()
-    replaced = []
-    for word in words:
-        replaced.append(giria_map.get(word, word))
+    replaced = [giria_map.get(word, word) for word in words]
     return ' '.join(replaced)
 
 
 def normalize_repeated_characters(text):
     return re.sub(r'(.)\1{2,}', r'\1', text)
-
 
 def remove_stopwords(text):
     stop_words = set(stopwords.words('portuguese'))
@@ -58,11 +54,10 @@ def remove_stopwords(text):
     filtered = [word for word in words if word not in stop_words]
     return ' '.join(filtered)
 
-
 def clean_non_textual_elements(text):
     text = re.sub(r'http\S+|www.\S+', '<url>', text)  
     text = re.sub(r'@\w+', '<mention>', text)         
-    text = re.sub(r'#\w+', '<hashtag>', text)        
+    text = re.sub(r'#\w+', '<hashtag>', text)         
     return text
 
 
@@ -71,20 +66,14 @@ giria_map = load_giria_map('giria_map.csv')
 
 
 def preprocess_text(text):
-    
     text = replace_emojis(text, emoji_map)
-  
     text = replace_girias(text, giria_map)
-    
     text = normalize_repeated_characters(text)
-    
     text = clean_non_textual_elements(text)
-   
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     text = text.lower()
     text = re.sub(r'\s+', ' ', text).strip()
-    
     text = remove_stopwords(text)
     return text
 
@@ -106,11 +95,15 @@ def classify():
     if not content:
         return jsonify({'error': 'Content is required'}), 400
 
+    
     preprocessed_content = preprocess_text(content)
     vectorized_content = text_to_bow(preprocessed_content, vocab)
     prediction = model.predict(vectorized_content)[0]
 
-    return jsonify({'classification': prediction})
+   
+    label = label_encoder.inverse_transform([prediction])[0]
+
+    return jsonify({'classification': label})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
